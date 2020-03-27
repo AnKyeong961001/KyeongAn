8c743b9ce9ea7f0937957bee3ceb4b64ecdbc40c
WOOSERK <55542546+WOOSERK@users.noreply.github.com>
2020-03-27 16:49:10 +0900

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,48 @@
+
+public class Student
+{
+	private String name;
+	private String number;
+	private String major;
+	private int score;
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setNumber(String number)
+	{
+		this.number = number;
+	}
+	
+	public void setMajor(String major)
+	{
+		this.major = major;
+	}
+	
+	public void setScore(int score)
+	{
+		this.score = score;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getNumber()
+	{
+		return number;
+	}
+	
+	public String getMajor()
+	{
+		return major;
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+}
