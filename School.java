7b14d59918c480fa666ad76b17896cefd084de0d
tlfzgjadyd <47634717+tlfzgjadyd@users.noreply.github.com>
2020-03-27 21:29:57 +0900

    Add files via upload

--- School.java
+++ School.java
@@ -138,53 +138,31 @@ public class School
 		}
 	}
 	
+	public static void pacade(School school, Set<Student> students) {
+		school.setPrintable("프린터");
+		printable.printStudent(students);
+		school.setPrintable("파일");
+		printable.printStudent(students);
+		school.setPrintable("콘솔");
+		printable.printStudent(students);
+		
+	}
+	
 	public static void main(String[] args)
 	{
 		School school = new School();	
 		Student s1 = new Student("gildong", "1", "soft", 100);
 		Student s2 = new Student("simchung", "2", "com", 90);
 		Student s3 = new Student("ganggam", "3", "soft", 80);
-		Student s4 = new Student("soonsin", "4", "com", 70);
+		Student s4 = new Student("soonsin", "4", "com", 100);
 		school.addStudent(s1);
 		school.addStudent(s2);
 		school.addStudent(s3);
 		school.addStudent(s4);
-		school.setPrintable("프린터");
-		printable.printStudent(school.getStudents());
-		school.setPrintable("파일");
-		printable.printStudent(school.getStudents());
-		school.setPrintable("콘솔");
-		printable.printStudent(school.getStudents());
-	
+		pacade(school, school.getStudents());
 		school.removeStudent("soonsin");
-		school.setPrintable("프린터");
-		printable.printStudent(school.getStudents());
-		school.setPrintable("파일");
-		printable.printStudent(school.getStudents());
-		school.setPrintable("콘솔");
-		printable.printStudent(school.getStudents());	
-		
-		school.setPrintable("프린터");
-		printable.printStudent(school.searchName("gildong"));
-		school.setPrintable("파일");
-		printable.printStudent(school.searchName("gildong"));
-		school.setPrintable("콘솔");
-		printable.printStudent(school.searchName("gildong"));
-		
-		
-		school.setPrintable("프린터");
-		printable.printStudent(school.searchMajor("soft"));
-		school.setPrintable("파일");
-		printable.printStudent(school.searchMajor("soft"));
-		school.setPrintable("콘솔");
-		printable.printStudent(school.searchMajor("soft"));
-		
-		
-		school.setPrintable("프린터");
-		printable.printStudent(school.searchNumber("2"));
-		school.setPrintable("파일");
-		printable.printStudent(school.searchNumber("2"));
-		school.setPrintable("콘솔");
-		printable.printStudent(school.searchNumber("2"));
+		pacade(school, school.searchName("gildong"));
+		pacade(school, school.searchMajor("soft"));
+		pacade(school, school.searchNumber("2"));
 	}
 }
