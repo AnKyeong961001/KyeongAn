57788277380cc99572703e1b433ba07193e89705
WOOSERK <55542546+WOOSERK@users.noreply.github.com>
2020-03-27 21:50:49 +0900

    Add files via upload

--- PrintByFile.java
+++ PrintByFile.java
@@ -3,23 +3,27 @@ import java.io.EOFException;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 public class PrintByFile implements Printable {
 
+	private Sortable sortable = new ScoreSort();
+	
 	@Override
 	public void printStudent(Set<Student> printStudents) {
 		
 		String path = null;
 		BufferedWriter out = null;
+		ArrayList<Student> al = sortable.sort(printStudents);
 		try {
-			out = new BufferedWriter(new FileWriter("/Users/jeonkyeong-an/temp/data3.txt"));
+			out = new BufferedWriter(new FileWriter("C:\\temp2\\data2.txt"));
 
 			while (true) {
 				try {
-					Iterator<Student> it = printStudents.iterator();
+					Iterator<Student> it = al.iterator();
 					while (it.hasNext()) {
 						Student student = it.next();
 						StringTokenizer st = new StringTokenizer(student.toString(), "/");
