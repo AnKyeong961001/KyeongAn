aac32a03ede8ed2982c8d68196fbdf9b1ccd6eae
WOOSERK <55542546+WOOSERK@users.noreply.github.com>
2020-03-27 19:03:33 +0900

    Add files via upload

---
+++ School.java
@@ -0,0 +1,147 @@
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class School
+{
+	private Set<Student> students;
+	private Printable printable;
+	
+	public School()
+	{
+		students = new TreeSet<Student>();
+		printable = null;
+	}
+	
+	// boolean을 반환하고 받는 곳에서 출력할지 출력하고 반환할지 결정해야 한다.
+	//
+	//
+	public boolean addStudent(Student student)
+	{
+		return students.add(student);
+	}
+	
+	public boolean removeStudent(String name)
+	{
+		Iterator<Student> is = students.iterator();
+		
+		while(is.hasNext())
+		{
+			Student student = is.next();
+			if(student.getName().equals(name))
+			{
+				students.remove(student);
+				System.out.println(name + " 삭제 완료");
+				return true;
+			}
+		}
+		
+		System.out.println(name + " 삭제 실패");
+		return false;
+	}
+	
+	public Set<Student> searchName(String name)
+	{
+		Set<Student> foundStudents = new TreeSet<>();
+		Iterator<Student> is = students.iterator();
+		
+		while(is.hasNext())
+		{
+			Student student = is.next();
+			if(student.getName().equals(name))
+			{
+				foundStudents.add(student);
+			}
+		}
+		
+		if(foundStudents.isEmpty())
+		{
+			return null;
+		}
+		else
+		{
+			return foundStudents;
+		}
+	}
+	
+	public Set<Student> searchMajor(String major)
+	{
+		Set<Student> foundStudents = new TreeSet<>();
+		Iterator<Student> is = students.iterator();
+		
+		while(is.hasNext())
+		{
+			Student student = is.next();
+			if(student.getMajor().equals(major))
+			{
+				foundStudents.add(student);
+			}
+		}
+		
+		if(foundStudents.isEmpty())
+		{
+			return null;
+		}
+		else
+		{
+			return foundStudents;
+		}
+	}
+	
+	public Set<Student> searchNumber(String number)
+	{
+		Set<Student> foundStudents = new TreeSet<>();
+		Iterator<Student> is = students.iterator();
+		
+		while(is.hasNext())
+		{
+			Student student = is.next();
+			if(student.getNumber().equals(number))
+			{
+				foundStudents.add(student);
+			}
+		}
+		
+		if(foundStudents.isEmpty())
+		{
+			return null;
+		}
+		else
+		{
+			return foundStudents;
+		}
+	}
+	
+	public Set<Student> getStudents()
+	{
+		return students;
+	}
+	
+	public void setPrintable(String type)
+	{
+		switch(type)
+		{
+		case "프린터":
+			printable = new PrintByPrinter();
+			System.out.println("출력 방식이 " + type +"으로 설정되었습니다.");
+			break;
+		case "파일":
+			printable = new PrintByFile();
+			System.out.println("출력 방식이 " + type +"으로 설정되었습니다.");
+			break;
+		case "콘솔":
+			printable = new PrintByConsole();
+			System.out.println("출력 방식이 " + type +"으로 설정되었습니다.");
+			break;
+		default:
+			System.out.println("잘못된 입력 방식입니다. 입력 : 프린터, 파일, 콘솔");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		School school = new School();
+		
+		
+	}
+}
--- Student.java
+++ Student.java
@@ -1,5 +1,5 @@
 
-public class Student
+public class Student implements Comparable<Student>
 {
 	private String name;
 	private String number;
@@ -58,5 +58,66 @@ public class Student
 	{
 		return "이름 : " + name + "/전공 : " + major + 
 				" /학번 : " + number + " 점수 " + score + "점";
+	}
+	
+	public boolean equals(Object o)
+	{
+		if(o == null) return false;
+		if(!(getClass().getName().equals(o.getClass().getName()))) return false;
+		
+		Student obj = (Student)o;
+		
+		// 이름 검사
+		if(name != null)
+		{
+			if(!(name.equals(obj.name))) return false;
+		}
+		else
+		{
+			if(obj.name != null) return false;
+		}
+		
+		// 학번 검사
+		if(number != null)
+		{
+			if(!(number.equals(obj.number))) return false;
+		}
+		else
+		{
+			if(obj.number != null) return false;
+		}
+		
+		// 전공 검사
+		if(major != null)
+		{
+			if(!(major.equals(obj.major))) return false; 
+		}
+		else
+		{
+			if(obj.major != null) return false;
+		}
+		// 점수 검사
+		if(score != obj.score) return false;
+		
+		return true;
+		
+	}
+	
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + name.hashCode();
+		result = prime * result + number.hashCode();
+		result = prime * result + major.hashCode();
+		result = prime * result + score;
+		
+		return result;
+	}
+	
+	// 점수로 비교해서 출력
+	public int compareTo(Student student)
+	{
+		return score - student.score;
 	}
 }
